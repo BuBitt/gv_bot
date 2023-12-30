57a802143f5f60607ea2293b3a9cdc90d90a8bf3
Quentao <quentao@gv.com>
2023-12-30 11:26:07 -0300

    removed unecessary code

--- views/profile.py
+++ views/profile.py
@@ -4,12 +4,3 @@ from discord.ext import commands
 
 
 logger = settings.logging.getLogger(__name__)
-
-
-class ProfileViewsCog(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-
-async def setup(bot):
-    await bot.add_cog(ProfileViewsCog(bot))
