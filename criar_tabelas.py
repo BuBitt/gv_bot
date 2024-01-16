6d137102a1beabe564a05ae69272aaa79f4b519e
Quentao <quentao@gv.com>
2024-01-16 09:02:53 -0300

    add new tables

--- criar_tabelas.py
+++ criar_tabelas.py
@@ -1,7 +1,20 @@
 from database import db
 from models.account import Account
+from models.donation import Donation
 from models.guild import Guild
+from models.mercado import MarketOffer
 from models.mercado_bis import MarketOfferBis
+from models.vendas import SellInfo
 from models.vendas_bis import SellInfoBis
 
-db.create_tables([MarketOfferBis, SellInfoBis, Account, Guild])
+db.create_tables(
+    [
+        MarketOfferBis,
+        SellInfoBis,
+        Account,
+        Guild,
+        Donation,
+        MarketOffer,
+        SellInfo,
+    ]
+)
